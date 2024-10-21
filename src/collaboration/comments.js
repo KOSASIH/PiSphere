6ecbe02330407ab25fb0dfbe6a94e66e655ab4f6
KOSASIH <kosasihg88@gmail.com>
2024-10-21 09:32:36 +0700

    Create comments.js

---
+++ src/collaboration/comments.js
@@ -0,0 +1,10 @@
+// src/collaboration/comments.js
+export function addComment(postId, comment) {
+    // Logic to add a comment to a post
+    console.log(`Comment added to post ${postId}: ${comment}`);
+}
+
+export function mentionUser (username) {
+    // Logic to mention a user
+    console.log(`User  ${username} has been mentioned.`);
+}
