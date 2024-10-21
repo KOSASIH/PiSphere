40eead1c3c096b25c89b8b6d65045c6c84c0a543
KOSASIH <kosasihg88@gmail.com>
2024-10-21 09:33:11 +0700

    Create customizableDashboard.js

---
+++ src/dashboard/customizableDashboard.js
@@ -0,0 +1,10 @@
+// src/dashboard/customizableDashboard.js
+export function createDashboard(userId) {
+    // Logic to create a new dashboard for the user
+    console.log(`Dashboard created for user ${userId}`);
+}
+
+export function addWidget(dashboardId, widget) {
+    // Logic to add a widget to a dashboard
+    console.log(`Widget added to dashboard ${dashboardId}: ${widget}`);
+}
