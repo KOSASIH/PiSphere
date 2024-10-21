d712504e63ac997ef73b88dc31ece588245b307d
KOSASIH <kosasihg88@gmail.com>
2024-10-21 09:35:09 +0700

    Create leaderboard.js

---
+++ src/gamification/leaderboard.js
@@ -0,0 +1,5 @@
+// src/gamification/leaderboard.js
+export function updateLeaderboard(userId, points) {
+    // Logic to update the leaderboard with new points
+    console.log(`User ${userId} has earned ${points} points.`);
+}
