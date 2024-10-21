1e86b09fd60fde9b658307da39c36d01e7ee5cee
KOSASIH <kosasihg88@gmail.com>
2024-10-21 09:33:50 +0700

    Create twoFactorAuth.js

---
+++ src/security/twoFactorAuth.js
@@ -0,0 +1,5 @@
+// src/security/twoFactorAuth.js
+export function enableTwoFactorAuth(userId) {
+    // Logic to enable 2FA for the user
+    console.log(`Two-factor authentication enabled for user ${userId}`);
+}
